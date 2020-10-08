89eb434447b741f5eb954d27a3108a1836cd195f
Ade Ayu Safitri <deyusf12@gmail.com>
2020-10-08 21:01:09 +0700

    Rewrite style to OsmiCSX

--- src/themes/CustomTheme.js
+++ src/themes/CustomTheme.js
@@ -1,3 +1,12 @@
 export default {
-    
+    // background
+    "bg-dark-100": { backgroundColor: "#e0e0e0" },
+
+    // font family
+    "mont-semi": { fontFamily: "Montserrat-SemiBold" },
+    "mont-regular": { fontFamily: "Montserrat" },
+
+    // width & height
+    "w-250": { width: 250 },
+    "h-150": { height: 150 },
 }
